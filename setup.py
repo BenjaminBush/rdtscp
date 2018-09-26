96be4d1d0b5e86d1129df5dece828b220193d7a5
Benjamin Bush <ben.bush4@gmail.com>
2018-09-26 16:39:56 -0500

    uploaded to pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from distutils.core import setup, Extension
+import setuptools
 
-rdtscp_module = Extension('rdtscp_module',
+rdtscp_module = setuptools.Extension('rdtscp_module',
                            sources = ['rdtscp_module.c'])
 
-setup(name = 'rdtscp_module',
+setuptools.setup(name = 'rdtscp_module',
       version = '1.0',
       description = 'C Extension for Python3 to call RDTSCP()',
-      ext_modules = [rdtscp_module])
+      ext_modules = [rdtscp_module],
+      author = "Benjamin Bush",
+      url= "https://github.com/BenjaminBush/rdtscp")
